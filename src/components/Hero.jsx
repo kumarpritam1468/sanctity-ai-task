107d9732c6c91c970785dadb935ebb5640af3366
Pritam <manoharipritam@gmail.com>
2024-11-02 16:46:18 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className=' pb-2 px-2 h-[calc(100svh-64px)]'>
+        
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className=' flex h-16 px-6 w-full justify-between items-center'>
+        <div>
+            <img src="/logo.png" alt="Sanctity" className=' invert w-40' />
+        </div>
+
+        <div>
+
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
